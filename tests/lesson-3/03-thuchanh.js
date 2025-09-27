eb6cb674cd34fab62c88901690b9f309d9ec49c2
PW - Tùng Anh <qstunganh@gmail.com>
2025-09-28 04:38:32 +0700

    lesson-3

---
+++ tests/lesson-3/03-thuchanh.js
@@ -0,0 +1,73 @@
+// //fuction: 
+// function sayHello() {
+//     console.log("Xin chao");
+// }
+
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+// //" in 3 lan: 1-->10, xin chao 
+// for (let i =1 ; i < = 10; i++){
+//     console.log(i);
+// }
+// console.log("Xin cHao");
+
+//fuction:
+function printNum(){
+    for (let i = 1 ; i <= 10; i++){
+    console.log(i);
+}
+}
+
+printNum();
+console.log("Xin cHao");
+printNum();
+console.log("Xin cHao");
+printNum();
+console.log("Xin cHao");
+
+
+    for (let i = 1 ; i <= 10; i++){
+    console.log(i);
+    }
+    console.log("Xin cHao");
+     for (let i = 1 ; i <= 10; i++){
+    console.log(i);
+    }
+    console.log("Xin cHao");
+     for (let i = 1 ; i <= 10; i++){
+    console.log(i);
+    }
+    console.log("Xin cHao");
+
+//return values
+
+// function getMax(a,b) {
+// if (a > b) {
+//     return(a);
+// }
+// return(b);
+// }
+// const max = getMax(20,15); //15
+// console.log(max);
+
+/*ulti fun*/
+
+// let str = "     HUYỀN GIÀ chuỗi có kí tự trống đầu và cuối     ";
+// console.log(str.trim());
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.includes("GIÀ"));
+// console.log(str.replace("GIÀ","NON"));
+// console.log(str.split("GIÀ"));
+// console.log(str.substring(20));
+// console.log(str.indexOf("GIÀ"));
+
+/*Array utils function*/
+let num = [1,2,3,4,5];
+let newmun = num.map(num => num * 2);
+console.log(`Đây là ${newmun} `);
+
+
